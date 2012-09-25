5e01c4811b59f9d67cb1066416b82f465a3cdf86
Takeharu Oshida <georgeOsdDev@gmail.com>
2012-09-25 21:20:14 +0900

    add script,css,ReadMe.md,sample.html

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+###　autoPrettify.js
+This script is a wrapper for [prettify.js](http://code.google.com/p/google-code-prettify/).
+prettify.js is very good tool, bat it needs some step to highlight syntax.
+So **autoPrettify.js** help it's step automatically.
+
+#### Usage
+just include this script on `head` tag
+
+    <script src="https://raw.github.com/georgeOsdDev/autoPrettify.js/master/autoPrettify.min.js">
---
+++ autoPrettify.js
@@ -0,0 +1,38 @@
+(function(){
+  // __name space__
+  window.autoPrettify = {
+    cdn:"https://raw.github.com/georgeOsdDev/autoPrettify.js/master",
+    jQueryTimerId:"",
+    prettifyTimerId:""
+  }
+  // load jquery
+  if(!window.jQuery) {
+    document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"><\/script>'); 
+  }
+  // wait until jquery was loaded
+  jQueryReady = function(tid){
+    if (window.jQuery) {
+      clearTimeout(tid);
+      $('head').append('<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Source+Code+Pro">');
+      $('head').append('<link rel="stylesheet" type="text/css" href="' + autoPrettify.cdn + '/assets/google-code-prettify/prettify.css">');
+      $('head').append('<link rel="stylesheet" type="text/css" href="' + autoPrettify.cdn + '/autoPrettify.css">');
+      $('head').append('<script type="text/javascript" src="' + autoPrettify.cdn + '/assets/google-code-prettify/prettify.js"><\/script>');
+      $('pre,code').each(function(){
+        $(this).addClass('prettyprint');
+      });
+      // wait until prettify.js was loaded
+      prettifyReady = function(tid2){
+        if (window.prettyPrint){
+          clearTimeout(tid2);
+          prettyPrint();
+        }
+      }
+      autoPrettify.prettifyTimerId = setInterval(function(){
+        prettifyReady(autoPrettify.prettifyTimerId)    
+      },100);
+    }
+  }
+  autoPrettify.jQueryTimerId = setInterval(function(){
+    jQueryReady(autoPrettify.jQueryTimerId)    
+  },100);
+})();
